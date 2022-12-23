56c102be11b32419c412ccfb160e6b10340d57ea
Ethan Ikegami <ikeg225@berkeley.edu>
2022-12-22 21:41:01 -0800

    blue

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,9 @@
 import styles from '../css/HomePage.module.css';
 import homepageShapes from '../images/homepage-shapes.png';
 import { NavLink } from 'react-router-dom';
+import topBlueCurve from '../images/topbluecurve.png';
+import bottomBlueCurve from '../images/bottombluecurve.png';
+import wordbubble from '../images/wordbubble.png';
 
 export default function HomePage() {
   return (
@@ -14,6 +17,19 @@ export default function HomePage() {
         <div className={styles.coverRight}>
           <img src={homepageShapes} alt="Homepage Shapes" />
         </div>
+      </div>
+      <div className={styles.bluesection}>
+        <img src={topBlueCurve} className={styles.topBlueCurve} />
+        <div className={styles.bluebackground}>
+          <div className={`${styles.blue} limitWidth`}>
+            <img src={wordbubble} />
+            <div className={styles.bluetext}>
+              <h1>What is Open Project?</h1>
+              <h3>We are a group of students striving to redefine tech club culture at Berkeley. Open Project is dedicated to creating a medium for passionate students to meet and work on projects that make a positive difference in the world - without the unnecessary selective application process.</h3>
+            </div>
+          </div>
+        </div>
+        <img src={bottomBlueCurve} className={styles.bottomBlueCurve} />
       </div>
     </div>
   );
