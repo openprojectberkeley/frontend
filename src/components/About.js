425e895340c4d1f91e3a5dd956119f7ec01bd1fe
Colin FitzGerald <colinfitzgerald328@gmail.com>
2023-01-02 22:38:51 -0800

    pushing mostly functional bottom half of about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,50 @@
 import styles from '../css/About.module.css';
+import orangeCurve from "../images/orangecurve.png";
+import colin from '../images/aboutPictures/colin.jpg';
+import nhu from '../images/aboutPictures/nhu.jpg';
+import uyen from '../images/aboutPictures/uyen.jpg';
+import ethan from '../images/aboutPictures/ethan.jpg';
+import nick from '../images/aboutPictures/nick.jpg';
+import joel from '../images/aboutPictures/joel.jpg';
+
 
 export default function About() {
     return (
       <div>
-        <h1>About</h1>
-      </div>
+        <img src={orangeCurve} className={styles.orangeCurve}></img>
+      <div className={`${styles.aboutBottomHalf} limitWidth`}>
+        <h1> Meet the Team </h1>
+      <div className={styles.imagesContainer}>
+        <div className={styles.topHalf}>
+        <div className={styles.person}>
+          <img className={styles.gridImage} src={ethan}></img>
+          <h3>Ethan Ikegami</h3>
+        </div>
+        <div className={styles.person}>
+          <img className={styles.gridImage} src={nick}></img>
+          <h3>Nick Litvinov</h3>
+        </div>
+        <div className={styles.person}>
+          <img className={styles.gridImage} src={joel}></img>
+          <h3>Joel Jaison</h3>
+        </div>
+        </div>
+       <div className={styles.bottomHalf}>
+       <div className={styles.person}>
+          <img className={styles.gridImage} src={colin}></img>
+          <h3>Colin FitzGerald</h3>
+        </div>
+        <div className={styles.person}>
+          <img className={styles.gridImage} src={nhu}></img>
+          <h3>Nhu Vu </h3>
+        </div>
+        <div className={styles.person}>
+          <img className={styles.gridImage} src={uyen}></img>
+          <h3>Uyen Phan </h3>
+        </div>
+       </div>
+    </div>
+  </div>
+  </div>
     );
-}
+  }
