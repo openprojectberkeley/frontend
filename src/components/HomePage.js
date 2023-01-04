fbddbdea0403b66423405a85c42f6cde2b2ff5b3
Ethan Ikegami <ikeg225@berkeley.edu>
2023-01-04 14:51:38 -0800

    homepage tweaks

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,14 +6,15 @@ import bottomBlueCurve from '../images/bottombluecurve.png';
 import wordbubble from '../images/wordbubble.png';
 import computer from '../images/computer.png';
 import threeIcons from '../images/threeIcons.png';
-import orangecurve from '../images/orangecurve1.png';
+import orangecurve from '../images/orangecurve.png';
 
 export default function HomePage() {
   return (
     <div>
       <div className={`${styles.cover} limitWidth`}>
         <div className={styles.coverLeft}>
-          <h1>Experience For All</h1>
+          <h1 className={styles.blueText}>Experience</h1>
+          <h1 className={styles.orangeText}>For All.</h1>
           <h2>We strive to build an open community for creators passionate about working on tech projects most meaningful to them.</h2>
           <NavLink to="/apply"><button className={styles.getInvolvedButton}>Get Involved</button></NavLink>
         </div>
@@ -34,35 +35,32 @@ export default function HomePage() {
         </div>
         <img src={bottomBlueCurve} className={styles.bottomBlueCurve} />
       </div>
-      <div className={styles.aboutProjects}>
-        <div className={styles.description1}>
-        <h1>Projects</h1>
-        <h3>The field of projects we work on range from web/mobile development to data science. Projects are <span className={styles.blueText}>student led projects</span> which are directed towards members that want to work on projects they find most intriguing - these can range from social good to general interesting projects. Additionally, we plan to expand to <span className={styles.blueText}>client projects</span> which will be for members that want to work on real-world projects for organizations and will vary based on company needs.
-        </h3>
+      <div className="limitWidth">
+        <div className={`${styles.about} ${styles.aboutProjects}`}>
+          <div>
+            <h1>Projects</h1>
+            <h3>The field of projects we work on range from web/mobile development to data science. Projects are <span className={styles.blueText}>student led projects</span> which are directed towards members that want to work on projects they find most intriguing - these can range from social good to general interesting projects. Additionally, we plan to expand to <span className={styles.blueText}>client projects</span> which will be for members that want to work on real-world projects for organizations and will vary based on company needs.</h3>
+          </div>
+          <img src={computer} />
         </div>
-        <img src={computer} />
-      </div>
-      <div className={styles.aboutCommunity}>
-        <img src={threeIcons} />
-        <div className={styles.description2}>
-        <h1>Community</h1>
-        <h3>Project teams will be with a <span className={styles.orangeText}>small group of students (5-7)</span> to encourage a tight knit community where team members can really get to know each other. 
-        Socials beyond general team meetings will vary by team such as after-meeting dinners or late night boba runs. In addition, there will be multiple club wide socials throughout the semester so you have the chance to meet everyone in the club!
-        </h3>
+        <div className={`${styles.about} ${styles.aboutCommunity}`}>
+          <img src={threeIcons} />
+          <div>
+            <h1>Community</h1>
+            <h3>Project teams will be with a <span className={styles.orangeText}>small group of students (5-7)</span> to encourage a tight knit community where team members can really get to know each other. Socials beyond general team meetings will vary by team such as after-meeting dinners or late night boba runs. In addition, there will be multiple club wide socials throughout the semester so you have the chance to meet everyone in the club!</h3>
+          </div>
         </div>
       </div>
-      <div className={styles.orangeCurve}>
-      <img src={orangecurve} />
-      </div>
-    
-      <div className={styles.joinUs}>
-        <div className={styles.joinUsText}>
-        <h1>Join us!</h1>
-        <h3>Application deadline is Wednesday, February 1st at 11:59 pm. Learn more using the button on the right!
-        </h3>
-        </div>
-        <div className={styles.joinUsRight}>
-          <NavLink to="/apply"><button className={styles.getInvolvedButton}>Get Involved</button></NavLink>
+      <img src={orangecurve} className={styles.orangeCurve}/>
+      <div className="limitWidth">
+        <div className={styles.joinUs}>
+          <div className={styles.joinUsText}>
+            <h1>Join us!</h1>
+            <h3>Application deadline is Wednesday, February 1st at 11:59 pm. Click the button to learn more!</h3>
+          </div>
+          <div className={styles.joinUsRight}>
+            <NavLink to="/apply"><button className={styles.getInvolvedButton}>Get Involved</button></NavLink>
+          </div>
         </div>
       </div>
     </div>
