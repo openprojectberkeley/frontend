a41002b518c97ebcaea3c50e487d9258b83bccdb
Nhu Vu <nhu@Nhus-MacBook-Pro.local>
2023-01-04 13:38:48 -0800

    finished bottom half of homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,8 +6,7 @@ import bottomBlueCurve from '../images/bottombluecurve.png';
 import wordbubble from '../images/wordbubble.png';
 import computer from '../images/computer.png';
 import threeIcons from '../images/threeIcons.png';
-import toporangecurve from '../images/toporangecurve.png';
-import bottomorangecurve from '../images/bottomorangecurve.png';
+import orangecurve from '../images/orangecurve1.png';
 
 export default function HomePage() {
   return (
@@ -38,7 +37,7 @@ export default function HomePage() {
       <div className={styles.aboutProjects}>
         <div className={styles.description1}>
         <h1>Projects</h1>
-        <h3>The field of projects we work on range from web/mobile development to data science. Projects are student led projects which are directed towards members that want to work on projects they find most intriguing - these can range from social good to general interesting projects. Additionally, we plan to expand to client projects which will be for members that want to work on real-world projects for organizations and will vary based on company needs.
+        <h3>The field of projects we work on range from web/mobile development to data science. Projects are <span className={styles.blueText}>student led projects</span> which are directed towards members that want to work on projects they find most intriguing - these can range from social good to general interesting projects. Additionally, we plan to expand to <span className={styles.blueText}>client projects</span> which will be for members that want to work on real-world projects for organizations and will vary based on company needs.
         </h3>
         </div>
         <img src={computer} />
@@ -47,14 +46,13 @@ export default function HomePage() {
         <img src={threeIcons} />
         <div className={styles.description2}>
         <h1>Community</h1>
-        <h3>Project teams will be with a small group of students (5-7) to encourage a tight knit community where team members can really get to know each other. 
+        <h3>Project teams will be with a <span className={styles.orangeText}>small group of students (5-7)</span> to encourage a tight knit community where team members can really get to know each other. 
         Socials beyond general team meetings will vary by team such as after-meeting dinners or late night boba runs. In addition, there will be multiple club wide socials throughout the semester so you have the chance to meet everyone in the club!
         </h3>
         </div>
       </div>
       <div className={styles.orangeCurve}>
-      <img src={toporangecurve} className={styles.topOrange} />
-      <img src={bottomorangecurve} className={styles.bottomOrange} />
+      <img src={orangecurve} />
       </div>
     
       <div className={styles.joinUs}>
