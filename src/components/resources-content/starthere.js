09d6194bc10fdeae021a3c33347582a747a024c1
Ethan Ikegami <ikeg225@berkeley.edu>
2023-04-09 12:54:46 -0700

    change start here page

--- src/components/resources-content/starthere.js
+++ src/components/resources-content/starthere.js
@@ -1,22 +1,22 @@
 const starthere = (
     <div>
         <h1>Stuck? Check these out!</h1>
-        <p>We've created some tutorials to help you learn more about each topic and guide you in the right direction. If you're looking for more detailed tutorials, don't hesitate to search on Google - there are plenty of resources available to help you dive deeper!</p>
-        <p>1. Git/GitHub</p>
-        <p>Any project that involves collaboration, version control, or sharing code with others would benefit from learning Git and GitHub. This could include software development projects, data science projects, research projects, and more.</p>
-        <p>2. HTML/CSS/JavaScript</p>
-        <p>Any project that involves building a website or web application would benefit from learning HTML, CSS, and JavaScript. This could include e-commerce sites, blogs, portfolio sites, social media platforms, and more.</p>
-        <p>3. Frontend (React)</p>
+        <p>We've created some tutorials to help you learn more about each topic and guide you in the right direction. Check them out below!</p>
+        <h3>1. Git/GitHub</h3>
+        <p>Any project that involves collaboration, version control, or sharing code with others would benefit from learning Git and GitHub.</p>
+        <h3>2. HTML/CSS/JavaScript</h3>
+        <p>Any project that involves building a website or web application would benefit from learning HTML, CSS, and JavaScript.</p>
+        <h3>3. Frontend (React)</h3>
         <p>Any project that involves building a dynamic and interactive user interface would benefit from learning React. This could include web applications that require real-time updates, dashboards that display complex data, or interactive tools that require user input.</p>
-        <p>4. Backend (Node)</p>
+        <h3>4. Backend (Node)</h3>
         <p>Any project that involves building a server-side application or API would benefit from learning Node.js. This could include web applications that require authentication, database integration, or complex business logic.</p>
-        <p>5. Full Stack (Django)</p>
-        <p>Any project that involves building a full-stack web application would benefit from learning Django. This could include e-commerce sites, social media platforms, productivity tools, and more.</p>
-        <p>6. Data Science</p>
-        <p>Any project that involves working with data would benefit from learning data science. This could include scientific research projects, business intelligence tools, marketing analytics, and more.</p>
-        <p>7. Computing</p>
+        <h3>5. Full Stack (Django)</h3>
+        <p>Any project that involves building a full-stack web application would benefit from learning Django.</p>
+        <h3>6. Data Science</h3>
+        <p>Any project that involves working with data would benefit from learning data science.</p>
+        <h3>7. Computing</h3>
         <p>Any project that involves large-scale data processing or machine learning would benefit from learning computing. By harnessing the power of modern computing technologies such as CPUs, GPUs, and specialized hardware, you can tackle complex problems and unlock new insights from your data.</p>
-        <p>8. Design</p>
+        <h3>8. Design</h3>
         <p>Any project that involves creating a visual or interactive experience for users would benefit from learning design principles. This could include websites, mobile apps, desktop apps, video games, and more.</p>
     </div>
 )
